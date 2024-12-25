325b5ab367b056926856cc18bb29c8dd53064840
yaalpha <70707470+yaalpha@users.noreply.github.com>
2024-12-25 18:46:18 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -93,7 +93,7 @@ sudo fc-cache -fv
 # Install Oh My Zsh
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
     echo "Installing Oh My Zsh..."
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
 # Copy configuration files
